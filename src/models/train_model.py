0c666b94a6a8432276723884192b02b7b7a86502
Moritz <moritz.steigerwald@icloud.com>
2022-06-17 10:55:18 +0200

    update

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from tqdm import tqdm
